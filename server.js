cc514cae14ef0f588bbc1eb5a4ae2e0e90db1b60
Johann Ingi <joi.ingi@me.com>
2018-04-15 13:22:35 +0200

    setup-2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 app = express();
 const PORT = 30000;
 
+const route = require('./routes/index');
+
 
 app.listen(PORT, () => {
     console.log('App listening on port: ' + PORT);
